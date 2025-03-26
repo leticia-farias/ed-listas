c9599cb5ef741e93d3e8583b2f40a15319613a11
leticia-farias <leticiafarias.engsoftware@gmail.com>
2025-03-26 18:01:50 -0300

    refactor: chamando os métodos remove e set

--- src/ListasApp.java
+++ src/ListasApp.java
@@ -3,13 +3,7 @@ public class ListasApp {
 
 	public static void main(String[] args) {
 		ListaEncadeada minhaLista=new ListaEncadeada();
-		// Testa métodos isEmpty, size, add e print
-	/*	if(minhaLista.isEmpty())
-			System.out.println("Lista Vazia");
-		else 
-			System.out.println("Size="+minhaLista.size());
-		minhaLista.print();
-			*/
+		
 		minhaLista.add("Jesus");
 		minhaLista.add("José");
 		minhaLista.add("Maria");
@@ -18,46 +12,22 @@ public class ListasApp {
 		minhaLista.add("Judas"); 
 		minhaLista.print();
 
-
-		/*
-		//testas os métodos remove
-		System.out.println("testando remover");
-		minhaLista.remove(3);
+		// testa os métodos remove
+		System.out.println();
+		System.out.println("removendo o elemento na posição 4");
+		minhaLista.remove(4);
 		minhaLista.print();
 		
-		System.out.println("testando outro remover");
-		minhaLista.remove("José");
+		System.out.println();
+		System.out.println("removendo o elemento 'Judas'");
+		minhaLista.remove("Judas");
 		minhaLista.print();
-		 */
 		
-		System.out.println("testando set");
+		// testa set
+		System.out.println();
+		System.out.println("subbstituindo o elemento na posição 1 por 'Joseph'");
 		minhaLista.set(1, "Joseph");
 		minhaLista.print();
 	}
 
 }
-
-/* 
-// testa indexOf		
-String aux="Pedro";
-System.out.println(minhaLista.indexOf(aux));
-
-
-// Testa get
-try {
-	System.out.println(minhaLista.get(8));
-}
-catch (IndexOutOfBoundsException ex) {
-	System.out.println(ex.getMessage());
-}	
-try {
-	System.out.println(minhaLista.get(2));
-}
-catch (IndexOutOfBoundsException ex) {
-	System.out.println(ex.getMessage());
-}
-
-// testa método add(index, object)
-minhaLista.add(4,"Mario");  			
-minhaLista.print();
-*/
